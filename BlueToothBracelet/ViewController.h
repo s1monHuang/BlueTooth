75da1833f24f9fc19f9cff30b40e936a573ca9a1
huang dengpin <allen@huangdeMacBook-Pro.local>
2015-07-02 22:50:39 +0800

    Initial Commit

---
+++ BlueToothBracelet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlueToothBracelet
+//
+//  Created by huang dengpin on 15/7/2.
+//  Copyright (c) 2015年 dachen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlueToothBraceletTests/BlueToothBraceletTests.m
@@ -0,0 +1,40 @@
+//
+//  BlueToothBraceletTests.m
+//  BlueToothBraceletTests
+//
+//  Created by huang dengpin on 15/7/2.
+//  Copyright (c) 2015年 dachen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BlueToothBraceletTests : XCTestCase
+
+@end
+
+@implementation BlueToothBraceletTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
