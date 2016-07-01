7520e458b787b9f6ddb8ab40865cff4689ee6d40
s1monHuang <azz@szxie.local>
2016-07-01 16:56:30 +0800

    增加关于我们界面

---
+++ Source/ViewController/Setting/AboutUsController.m
@@ -0,0 +1,27 @@
+//
+//  AboutUsController.m
+//  BlueToothBracelet
+//
+//  Created by azz on 16/7/1.
+//  Copyright © 2016年 dachen. All rights reserved.
+//
+
+#import "AboutUsController.h"
+
+@implementation AboutUsController
+
+- (void)viewDidLoad
+{
+    self.title = @"关于我们";
+    self.view.backgroundColor = kThemeGrayColor;
+    UILabel *versionLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, kScreenHeight*0.5 - 60, kScreenWidth, 40)];
+    versionLabel.text = @"当前版本：1.0.0";
+    versionLabel.font = [UIFont systemFontOfSize:18];
+    versionLabel.textColor = [UIColor lightGrayColor];
+    versionLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:versionLabel];
+    
+}
+
+
+@end
